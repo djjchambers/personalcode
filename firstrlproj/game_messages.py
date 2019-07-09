e1dcde7c6f970c67ab0caa618dfa490d9ac141b8
Dan Chambers <djjchambers@gmail.com>
2019-07-09 22:08:46 +0100

    tcod tutorial part 7

---
+++ firstrlproj/game_messages.py
@@ -0,0 +1,29 @@
+import tcod as libtcod
+
+import textwrap
+
+
+class Message:
+    def __init__(self, text, color=libtcod.white):
+        self.text = text
+        self.color = color
+        
+class MessageLog:
+    def __init__(self, x, width, height):
+        self.messages = []
+        self.x = x
+        self.width = width
+        self.height = height
+        
+    def add_message(self, message):
+        # Split message if necessary along multiple lines
+        new_msg_lines = textwrap.wrap(message.text, self.width)
+        
+        for line in new_msg_lines:
+            # If buffer is full, remove 1st line to make room for new one
+            if len(self.messages) == self.height:
+                del self.messages[0]
+                
+            # Add new line as a Message obj, with text and color
+            self.messages.append(Message(line, message.color))
+            
